f58d5a578b8f0dd73f5e465f95a49bb5503ea424
Max <4649120+maximehk@users.noreply.github.com>
2025-03-06 23:04:48 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+// Fast I/O (reduces overhead)
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+
+using namespace std;
+
+
+#define ll long long
+#define ld long double
+#define endl "\n" // Avoids flushing buffers every time
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define fi first
+#define se second
